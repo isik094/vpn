d0045d96654c80951035600a560f35f94547de61
Nasirov Ismaiil <nasirov-ismaiil@yandex.ru>
2025-06-16 01:54:57 +0300

    feat(): fix

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PaymentController;
 use App\Http\Middleware\CheckWebhookIp;
 use App\Http\Middleware\VerifyWataSignature;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::post('/payment/callback', [PaymentController::class, 'callback'])
+    ->withoutMiddleware([VerifyCsrfToken::class])
     ->middleware(CheckWebhookIp::class)
     ->middleware(VerifyWataSignature::class);

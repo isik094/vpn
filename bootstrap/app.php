9b568763a088546f9a1e0ba05db3e694840d28f3
Nasirov Ismaiil <nasirov-ismaiil@yandex.ru>
2025-06-16 01:49:22 +0300

    feat(): fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
-            'payment/*',
+            'payment/callback',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {

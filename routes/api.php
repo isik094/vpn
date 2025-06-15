f373377938274439cc36b90c6a7f290ea0e6f650
Nasirov Ismaiil <nasirov-ismaiil@yandex.ru>
2025-06-16 02:23:05 +0300

    feat(): fix

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\PaymentController;
 use App\Http\Middleware\CheckWebhookIp;
 use App\Http\Middleware\VerifyWataSignature;
 
-Route::post('/payment/callback', [PaymentController::class, 'callback']);
-//    ->middleware(CheckWebhookIp::class)
-//    ->middleware(VerifyWataSignature::class);
+Route::post('/payment/callback', [PaymentController::class, 'callback'])
+    ->middleware(CheckWebhookIp::class)
+    ->middleware(VerifyWataSignature::class);
 
